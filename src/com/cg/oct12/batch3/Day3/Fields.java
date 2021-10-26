ded20a516f90225178a5ccf67932d0f8e31f182f
Urvesh <90395969+Urvessss@users.noreply.github.com>
2021-10-26 09:34:04 +0530

    Update Fields.java
    Commit

--- src/com/cg/oct12/batch3/Day3/Fields.java
+++ src/com/cg/oct12/batch3/Day3/Fields.java
@@ -1,20 +1,20 @@
 package com.cg.oct12.batch3.Day3;
 
 public class Fields {
-	//static field
+	// static field
 	static int num1;
-	//non static field
+	// non static field
 	int num2;
-	
-public static void main(String[] args) {
-	int num3;
-    num3=3;
-	System.out.println(num3);
-	
-	System.out.println(Fields.num1);
-	
-	Fields F1=new Fields();
-	System.out.println(F1.num2);
-	
-}
+
+	public static void main(String[] args) {
+		int num3;
+		num3 = 3;
+		System.out.println(num3);
+
+		System.out.println(Fields.num1);
+
+		Fields F1 = new Fields();
+		System.out.println(F1.num2);
+
+	}
 }
